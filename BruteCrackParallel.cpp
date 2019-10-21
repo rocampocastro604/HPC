b1f0fa413d70106f1dc6b3419f202e3aa32fbf11
danidehs <hsdaniele95@gmail.com>
2019-10-21 00:36:43 -0500

    Fix

--- BruteCrackParallel.cpp
+++ BruteCrackParallel.cpp
@@ -16,11 +16,10 @@ char chars[]={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g
 
 string Crack (string pass) {
     length = sizeof(chars);
-    #pragma omp parallel for private (cracked) schedule (guided)
+    #pragma omp parallel for private (cracked) schedule (static)
     for(int i = 0; i <= length; i++) {
         if(pass == temporal) exit;
         cracked = cracked + chars[i];
-        
         for(int j = 0; j <= length; j++) {
             if (pass == temporal) break;
             cracked = cracked + chars[j];
