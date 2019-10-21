5df953e088bdfd816afe910dad5f07fee87dc775
rocampocastro604 <44033716+rocampocastro604@users.noreply.github.com>
2019-10-20 23:56:16 -0500

    Pruebas
    Versus betwwen Serial and Multiprocessing

---
+++ muestra.py
@@ -0,0 +1,40 @@
+import time
+from multiprocessing import Pool
+
+def sum_square(number):
+    s = 0
+    for i in range(number):
+        s += i * i
+    return s
+
+
+def sum_square_with_mp(numbers):
+
+    start_time = time.time()
+    p = Pool()
+    result = p.map(sum_square, numbers)
+
+    p.close()
+    p.join()
+
+    end_time = time.time() - start_time
+
+    print("Processing", len(numbers), "numbers took" ,end_time ,"time using multiprocessing.")
+
+
+def sum_square_no_mp(numbers):
+
+    start_time = time.time()
+    result = []
+
+    for i in numbers:
+        result.append(sum_square(i))
+    end_time = time.time() - start_time
+
+    print("Processing", len(numbers), "numbers took" ,end_time , "time using serial processing.")
+
+
+if __name__ == '__main__':
+    numbers = range(100)
+    sum_square_with_mp(numbers)
+    sum_square_no_mp(numbers)
